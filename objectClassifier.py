8aed456cb193ed01a884148cfcbc1cba2a5d8cfa
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-12-08 14:38:13 -0500

    testing

--- objectClassifier.py
+++ objectClassifier.py
@@ -139,8 +139,7 @@ FRAME_WINDOW = st.image([])
 camera = cv2.VideoCapture(0)
 
 while run:
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = cv2.cvtColor(camera.read()[1], cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
 else:
     st.write('Stopped')
