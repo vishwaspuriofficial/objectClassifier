3f19aa04a82b84050920ff916c556f0879a1323a
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-12-09 14:24:29 -0500

    Updated Files

--- objectClassifier.py
+++ objectClassifier.py
@@ -82,25 +82,12 @@
 #     st.write("2. Slide the camera slider to on")
 #     st.write("3. Reload your page!")
 
-import numpy as np
-import cv2
+import streamlit as st
+from webcam import webcam
 
-cap = cv2.VideoCapture(0)
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
-
-
+captured_image = webcam()
+if captured_image is None:
+    st.write("Waiting for capture...")
+else:
+    st.write("Got an image from the webcam:")
+    st.image(captured_image)
