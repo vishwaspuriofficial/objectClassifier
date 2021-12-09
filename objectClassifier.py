a0175832ab3d520e23b4d689eb178cbf4c799033
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-12-09 13:57:35 -0500

    Updated Files

--- objectClassifier.py
+++ objectClassifier.py
@@ -83,17 +83,23 @@
 #     st.write("3. Reload your page!")
 
 import streamlit as st
-import cv2
+import cv2 as cv
 
-cap = cv2.VideoCapture(0)
+@st.cache(allow_output_mutation=True)
+def get_cap():
+    return cv.VideoCapture(0)
+
+cap = get_cap()
 
 frameST = st.empty()
+param=st.sidebar.slider('chose your value')
 
 while True:
     ret, frame = cap.read()
     # Stop the program if reached end of video
     if not ret:
         print("Done processing !!!")
+        cv.waitKey(3000)
         # Release device
         cap.release()
         break
