d3c63d03966828a6a595589e6c66215cbd6ebf17
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-12-09 13:16:06 -0500

    Updated Files

--- objectClassifier.py
+++ objectClassifier.py
@@ -25,76 +25,6 @@ net.setInputScale(1.0/ 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-from streamlit_webrtc import (
-    AudioProcessorBase,
-    RTCConfiguration,
-    VideoProcessorBase,
-    WebRtcMode,
-    webrtc_streamer,
-)
-try:
-    from typing import Literal
-except ImportError:
-    from typing_extensions import Literal  # type: ignore
-import av
-
-RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-)
-st.write("Press start to turn on Camera!")
-st.write("If camera doesn't turn on, click the select device button, change the camera input and reload your screen!")
-
-col = st.empty()
-
-start = col.button('Start')
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
-
-camOn = False
-
-if start:
-    with st.spinner('Turning camera on!'):
-        time.sleep(3)
-    stop = col.button("Stop")
-    camOn = True
-
-
-while camOn:
-    _, frame = camera.read()
-    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    classIds, confs, bbox = net.detect(frame, confThreshold=thres)
-    if len(classIds) != 0:
-
-        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-            print(classNames[classId-1])
-            if confidence > 0.6:
-                cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-                cv2.putText(img, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30),
-                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-                # cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
-                # cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-                # Show Confidence Level
-    FRAME_WINDOW.image(img)
-    if stop:
-        st.success("Turning camera off!")
-        break
-
-# else:
-#     st.write('Camera Stopped!')
-
-
-
-
-
-
-
-
-
-import cv2
-import streamlit as st
-import time
-
 
 col = st.empty()
 
@@ -113,11 +43,20 @@ if start:
 
 while camOn:
     ret, frame = camera.read()
-    FRAME_WINDOW.image(frame)
-    # img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    #     cv2.rectangle(img, (5,5),(100,100), color=(0, 255, 0), thickness=2)
-    #         cv2.putText(img, "dsa", (100,100),
-    #                     cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+    if ret:
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        classIds, confs, bbox = net.detect(frame, confThreshold=thres)
+        if len(classIds) != 0:
+
+            for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+                if confidence > 0.6:
+                    cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+                    cv2.putText(img, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                    # cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                    # cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    # Show Confidence Level
+        FRAME_WINDOW.image(img)
     if stop:
         break
 
