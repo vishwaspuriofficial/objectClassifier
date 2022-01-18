f3a8c58d11080116f0e3ee2e62028f8abcca98b7
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-01-18 04:44:19 -0500

    updated comments

--- objectClassifier.py
+++ objectClassifier.py
@@ -1,7 +1,15 @@
+#Title: OBJECT CLASSIFIER
+#Developer: Vishwas Puri
+#Purpose: A program that uses your webcam to find and detect many objects, shown in your camera.
+
+#It uses Mobile net SSD and a famous categorized data set called “coco.names”.
+
+#This program is made using python supported by streamlit.
 import streamlit as st
 import mediapipe as mp
 import cv2
-import time
+
+#defined the face recognition model
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 st.set_page_config(layout="wide")
 
@@ -23,19 +31,23 @@ RTC_CONFIGURATION = RTCConfiguration(
 )
 st.write("Press start to turn on camera and show it some objects!")
 
+#defining mediapipe's inbuilt pose recogignition models
 thres = 0.45  # Threshold to detect object
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 mpDraw = mp.solutions.drawing_utils
 
+#getting all the cateogarized names from the coco.names data set
 classNames = []
 classFile = "coco.names"
 with open(classFile, 'rt') as f:
     classNames = f.read().split('\n')
 
+#the classification models are being defined
 configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 weightsPath = 'frozen_inference_graph.pb'
 
+#defining the quality of the detection using its weight and config files, respectively
 net = cv2.dnn_DetectionModel(weightsPath, configPath)
 net.setInputSize(320, 320)
 net.setInputScale(1.0 / 127.5)
@@ -46,10 +58,10 @@ def objectClassifier():
     class OpenCVVideoProcessor(VideoProcessorBase):
         def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
             img = frame.to_ndarray(format="bgr24")
-
+            #get details of the detection
             classIds, confs, bbox = net.detect(img, confThreshold=thres)
             if len(classIds) != 0:
-
+                #draw a rectangle over the object and write its catogarized name over it
                 for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
                     if confidence > 0.6:
                         cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
@@ -62,7 +74,7 @@ def objectClassifier():
 
             return av.VideoFrame.from_ndarray(img, format="bgr24")
 
-
+    # setting up streamlit camera configuration
     webrtc_ctx = webrtc_streamer(
         key="opencv-filter",
         mode=WebRtcMode.SENDRECV,
@@ -88,3 +100,5 @@ def objectClassifier():
     st.write("'Coco.names' Data List, https://github.com/pjreddie/darknet/blob/master/data/coco.names")
 if __name__ == "__main__":
     objectClassifier()
+
+
